3c02e28db29e2d1568bbd3764b1d2d6b83828170
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-01-12 17:33:21 +0500

    location topbar

--- src/component/Map.js
+++ src/component/Map.js
@@ -13,24 +13,19 @@ const {width, height} = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
 
 const Map = () => (
-  <View style={styles.container}>
-    <MapView
-      provider={PROVIDER_GOOGLE}
-      style={styles.absolute}
-      initialRegion={{
-        latitude: 24.8659854,
-        longitude: 67.1735312,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01 * ASPECT_RATIO,
-      }}
-      customMapStyle={mapStyle}></MapView>
-  </View>
+  <MapView
+    provider={PROVIDER_GOOGLE}
+    style={styles.absolute}
+    initialRegion={{
+      latitude: 24.8659854,
+      longitude: 67.1735312,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01 * ASPECT_RATIO,
+    }}
+    customMapStyle={mapStyle}></MapView>
 );
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   absolute: {
     ...StyleSheet.absoluteFillObject,
     flex: 1,
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,81 @@
 import React from 'react';
 
-import {View, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 // Components
 import Map from '../component/Map';
 
+// Icons
+import Feather from 'react-native-vector-icons/Feather';
+
 const Home = () => (
   <View style={styles.container}>
     <Map />
+
+    <SafeAreaView style={styles.safeContainer}>
+      <View>
+        <Feather name="menu" size={24} />
+      </View>
+      <TouchableOpacity style={styles.search}>
+        <View style={styles.inputWrapper}>
+          <View style={styles.greenDot} />
+          <View>
+            <Text style={styles.inputText}>What your Location</Text>
+          </View>
+        </View>
+        <View>
+          <Feather name="heart" size={20} style={{color: '#8b8d96'}} />
+        </View>
+      </TouchableOpacity>
+    </SafeAreaView>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  safeContainer: {
+    marginHorizontal: 20,
+  },
+  search: {
+    marginVertical: 20,
+    padding: 15,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 5,
+
+    elevation: 3,
+  },
+  greenDot: {
+    width: 10,
+    height: 10,
+    backgroundColor: '#0ddda2',
+    borderRadius: 100,
+    marginRight: 10,
+  },
+  inputText: {
+    fontWeight: '600',
+    color: '#8b8d96',
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
